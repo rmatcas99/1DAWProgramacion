019d3be8a15a3283bfe99e3234689089966e07a2
Rubén Mata Castro <71704314+rmatcas99@users.noreply.github.com>
2021-01-19 09:32:42 +0100

    19/01

---
+++ src/capitulo4/Cuadro.java
@@ -0,0 +1,36 @@
+package capitulo4;
+
+import java.awt.BorderLayout;
+import java.awt.Canvas;
+import java.awt.Color;
+import java.awt.Graphics;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class Cuadro extends Canvas {
+	
+	public static final int WIDTH = 700;
+	public static final int HEIGHT = 700;
+	
+	JFrame ventana = new JFrame("Ejercicio4");
+	
+	public Cuadro() {
+		
+		JPanel panel = (JPanel) ventana.getContentPane();
+		ventana.setBounds(0, 0, WIDTH, HEIGHT);
+		panel.setLayout(new BorderLayout());
+		panel.add(this, BorderLayout.CENTER);
+		ventana.setVisible(true);
+	}
+	
+	public void paint(Graphics g) {
+
+		g.setColor(Color.RED);
+		g.fillRect(50, 50, 130, 120);
+		
+		g.setColor(Color.GREEN);
+		g.fillOval(500, 500, 80, 120);
+	}
+
+}
---
+++ src/capitulo4/Pintura.java
@@ -0,0 +1,16 @@
+package capitulo4;
+
+import java.awt.Color;
+import java.awt.Graphics;
+
+public class Pintura {
+
+	public static void main(String[] args) {
+
+		Cuadro formas = new Cuadro();
+
+		
+
+	}
+
+}
