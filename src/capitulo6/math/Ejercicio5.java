2980daa1eac089e140f9cecc5c262b67a4cb4de9
Rubén Mata Castro <71704314+rmatcas99@users.noreply.github.com>
2021-01-29 10:12:23 +0100

    5

--- src/capitulo6/math/Ejercicio5.java
+++ src/capitulo6/math/Ejercicio5.java
@@ -3,10 +3,33 @@ package capitulo6.math;
 public class Ejercicio5 {
 
 	public static void main(String[] args) {
-		double x = 0.001;
-		double y;
+		double x = 0.0001;
+		double y, y1, y2, fin = 0;
+
+//		y = x * Math.sin(x);
+//		System.out.println(y);
+//		y1 = (x - 0.0001) * Math.sin(x);
+//		System.out.println(y1);
+//		y2 = (x + 0.0001) * Math.sin(x);
+//		System.out.println(y2);
 		
 		
+		do {
+			y = x * Math.sin(x);
+			//System.out.println(y);
+			y1 = (x - 0.0001) * Math.sin(x);
+			//System.out.println(y);
+			y2 = (x + 0.0001) * Math.sin(x);
+			//System.out.println(y);
+
+			if (y > y1 && y > y2) {
+				System.out.println(y);
+				fin++;
+			}
+			
+			x += 0.001;
+
+		} while (fin < 4);
 
 	}
 
