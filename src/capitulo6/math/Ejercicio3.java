dab9848c7c534bb32b26ce243dbbf2e0c97ccc97
Rubén Mata Castro <71704314+rmatcas99@users.noreply.github.com>
2021-01-31 18:39:48 +0100

    capitulo6

--- src/capitulo6/math/Ejercicio3.java
+++ src/capitulo6/math/Ejercicio3.java
@@ -3,7 +3,20 @@ package capitulo6.math;
 public class Ejercicio3 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	
+		double x = 3;
+		double pi = 4;
+		int signo = -1;
+		
+		do {
+			
+			pi += signo * 4.0 / x;
+			signo *= -1;
+			x += 2;
+			
+		} while(Math.abs(Math.PI - pi) >= 0.00001);
+		
+		System.out.println(pi);
 
 	}
 
