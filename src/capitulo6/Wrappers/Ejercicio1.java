242a64fe591267f9dcb40d3e26a15fc781bae0bf
Rubén Mata Castro <71704314+rmatcas99@users.noreply.github.com>
2021-02-09 12:50:39 +0100

    capitulo6

---
+++ src/capitulo6/Wrappers/Ejercicio1.java
@@ -0,0 +1,15 @@
+package capitulo6.Wrappers;
+
+public class Ejercicio1 {
+
+	public static void main(String[] args) {
+		// Realizar un programa que imprima una tabla con los límites máximo y mínimo
+		// representables para los diferentes tipos primitivos en Java (byte, int,
+		// short, long, float, double). En la tabla también deberá aparecer el número de
+		// bytes que cada tipo usa para su representación.
+		
+		
+
+	}
+
+}
---
+++ src/capitulo6/Wrappers/Ejercicio2.java
@@ -0,0 +1,10 @@
+package capitulo6.Wrappers;
+
+public class Ejercicio2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
