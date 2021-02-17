efa4abb94d6ae07c77dc509ac06020276451781f
Rubén Mata Castro <71704314+rmatcas99@users.noreply.github.com>
2021-02-17 13:22:08 +0100

    3enRaya

--- src/tresEnRaya/CuadroDeJuego.java
+++ src/tresEnRaya/CuadroDeJuego.java
@@ -9,22 +9,117 @@ public class CuadroDeJuego {
 	private int esquinaSuperiorX, esquinaSuperiorY;
 	private int ancho, alto;
 	
+	private int estado = 0;
+	
 	public CuadroDeJuego(int xTablero, int yTablero) {
 		super();
 		this.xTablero = xTablero;
 		this.yTablero = yTablero;
 	}
 	
-	public void paint (Graphics g) {
+	public void paint(Graphics g) {
 		
 		ancho = TresEnRaya.getInstance().getWidth() / 3;
 		alto = TresEnRaya.getInstance().getHeight() / 3;
 		
 		esquinaSuperiorX = this.xTablero * ancho;
 		esquinaSuperiorY = this.yTablero * alto + 2;
-		
+
 		g.setColor(Color.WHITE);
 		g.drawRect(esquinaSuperiorX, esquinaSuperiorY, ancho, alto);
+
+		if (estado == 1) {
+			g.setColor(Color.GREEN);
+			g.drawOval(esquinaSuperiorX, esquinaSuperiorY, ancho, alto);
+			
+		}
+
+		if (estado == 2) {
+
+			g.setColor(Color.BLUE);
+			g.drawLine(esquinaSuperiorX, esquinaSuperiorY, esquinaSuperiorX + ancho, esquinaSuperiorY + alto);
+			g.drawLine(esquinaSuperiorX + ancho, esquinaSuperiorY, esquinaSuperiorX, esquinaSuperiorY + alto);
+		}
 	}
+	
+	public boolean clicEnCuadro(int clicX, int clicY) {
+
+		if (clicX > this.esquinaSuperiorX && clicX < (this.esquinaSuperiorX + this.ancho) && clicY > this.esquinaSuperiorY
+				&& clicY < (this.esquinaSuperiorY + this.alto)) {
+			return true;
+
+		}
+		return false;
+	}
+
+	public void clic() {
+		int turno = TresEnRaya.getInstance().getTurno();
+		if (TresEnRaya.getInstance().entradaTablero(xTablero, yTablero)) {
+			if (turno == 1) {
+				this.estado = 1;
+			}
+			if (turno == 2) {
+				this.estado = 2;
+			}
+		}
+	}
+
+	public int getxTablero() {
+		return xTablero;
+	}
+
+	public void setxTablero(int xTablero) {
+		this.xTablero = xTablero;
+	}
+
+	public int getyTablero() {
+		return yTablero;
+	}
+
+	public void setyTablero(int yTablero) {
+		this.yTablero = yTablero;
+	}
+
+	public int getEsquinaSuperiorX() {
+		return esquinaSuperiorX;
+	}
+
+	public void setEsquinaSuperiorX(int esquinaSuperiorX) {
+		this.esquinaSuperiorX = esquinaSuperiorX;
+	}
+
+	public int getEsquinaSuperiorY() {
+		return esquinaSuperiorY;
+	}
+
+	public void setEsquinaSuperiorY(int esquinaSuperiorY) {
+		this.esquinaSuperiorY = esquinaSuperiorY;
+	}
+
+	public int getAncho() {
+		return ancho;
+	}
+
+	public void setAncho(int ancho) {
+		this.ancho = ancho;
+	}
+
+	public int getAlto() {
+		return alto;
+	}
+
+	public void setAlto(int alto) {
+		this.alto = alto;
+	}
+
+	public int getEstado() {
+		return estado;
+	}
+
+	public void setEstado(int estado) {
+		this.estado = estado;
+	}
+	
+	
 	
 }
--- src/tresEnRaya/TresEnRaya.java
+++ src/tresEnRaya/TresEnRaya.java
@@ -5,6 +5,8 @@ import java.awt.Canvas;
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Toolkit;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import java.util.ArrayList;
@@ -12,6 +14,7 @@ import java.util.List;
 
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
+import javax.swing.JPanel;
 
 public class TresEnRaya extends Canvas {
 
@@ -20,23 +23,50 @@ public class TresEnRaya extends Canvas {
 	public static final int ANCHO = 600;
 	public static final int ALTO = 600;
 	public static TresEnRaya instancia = null;
+	private int turno = 1;
+	private int [][] tablero = { { 0, 0, 0}, { 0, 0, 0}, { 0, 0, 0} };
 
 	public TresEnRaya() {
 		
 		ventana = new JFrame("3 en Raya");
-		ventana.setBounds(0, 0, ANCHO, ALTO);
 		
-		ventana.getContentPane().setLayout(new BorderLayout());
-		ventana.getContentPane().add(this, BorderLayout.CENTER);
+		JPanel panel = (JPanel) ventana.getContentPane();
+		panel.setLayout(new BorderLayout());
+		panel.add(this, BorderLayout.CENTER);
+		
+		ventana.setBounds(0, 0, ANCHO, ALTO);
 		
 		creaCuadros();
 		
+		this.addMouseListener(new MouseAdapter() {
+
+			@Override
+			public void mouseClicked(MouseEvent e) {
+
+				super.mouseClicked(e);
+				repaint();
+
+				if (e.getButton() == MouseEvent.BUTTON1) {
+
+					for (CuadroDeJuego c : cuadros) {
+						if (c.clicEnCuadro(e.getX(), e.getY())) {
+							c.clic();
+							resultado();
+						}
+					}
+
+				}
+			}
+
+		});
+		
 		ventana.setResizable(false);
-		ventana.setVisible(true);
-		ventana.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
 		
 		this.requestFocus();
 		
+		ventana.setVisible(true);
+		ventana.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+				
 		ventana.addWindowListener(new WindowAdapter() {
 			@Override
 			public void windowClosing(WindowEvent e) {
@@ -46,15 +76,14 @@ public class TresEnRaya extends Canvas {
 		
 	}
 	
-	public static List<CuadroDeJuego> creaCuadros() {
+	public void creaCuadros() {
 		
-		for (int i = 0; i < 3; i++) {
-			for (int j = 0; j < 3; j++) {
-				cuadros.add(new CuadroDeJuego(j, i));
+		for (int i = 0; i < tablero.length; i++) {
+			for (int j = 0; j < tablero[i].length; j++) {
+				CuadroDeJuego cuadro = new CuadroDeJuego(j, i);
+				cuadros.add(cuadro);
 			}
 		}
-		
-		return cuadros;
 	}
 	
 	@Override
@@ -65,14 +94,15 @@ public class TresEnRaya extends Canvas {
 		
 		g.setColor(Color.BLACK);
 		g.fillRect(0, 0, this.getWidth(), this.getHeight());
-		
 		for (CuadroDeJuego cuadroDeJuego : cuadros) {
-			cuadroDeJuego.paint(g);			
+			g.setColor(Color.WHITE);
+			cuadroDeJuego.paint(g);		
+			
 		}
 	}
 
 	public static void main(String[] args) {
-		TresEnRaya.getInstance();
+		TresEnRaya.getInstance().creaCuadros();
 	}
 
 	public static TresEnRaya getInstance() {
@@ -88,6 +118,97 @@ public class TresEnRaya extends Canvas {
 				JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE, null, opciones, "Aceptar");
 		if (eleccion == JOptionPane.YES_OPTION) {
 			System.exit(0);
+		}
+	}
+	
+	public boolean entradaTablero(int x, int y) {
+
+		boolean valida = true;
+
+		for (int i = y; i < tablero.length; i++) {
+			for (int j = x; j < tablero.length; j++) {
+				if (tablero[i][j] != 0) {
+					//JOptionPane.showMessageDialog(instancia, "Cuadro Ocupado");
+					valida = false;
+					break;
+				}
+				if (valida) {
+					if (turno == 1) {
+						tablero[i][j] = 1;
+						turno = 2;
+						break;
+					}
+					if (turno == 2) {
+						tablero[i][j] = 2;
+						turno = 1;
+						break;
+					}
+				}
+			}
+			break;
+		}
+
+		return valida;
+	}
+
+	public int getTurno() {
+		return turno;
+	}
+
+	public void setTurno(int turno) {
+		this.turno = turno;
+	}
+
+	private static int getGanadorDelTablero(int tablero[][]) {
+		
+		for (int i = 0; i < tablero.length; i++) {
+			if (tablero[i][0] == tablero[i][1] && tablero[i][1] == tablero[i][2]) {
+				return tablero[i][0]; 
+			}
+		}
+		
+		for (int i = 0; i < tablero[0].length; i++) {
+			if (tablero[0][i] == tablero[1][i] && tablero[1][i] == tablero[2][i]) {
+				return tablero[0][i]; 
+			}
+		}
+		
+		if (tablero[0][0] == tablero[1][1] && tablero[1][1] == tablero[2][2]) {
+			return tablero[0][0]; 
+		}
+		
+		if (tablero[0][2] == tablero[1][1] && tablero[1][1] == tablero[2][0]) {
+			return tablero[0][2]; 
+		}
+		
+		boolean hayCasillasVacias = false;
+		for (int i = 0; i < tablero.length; i++) {
+			for (int j = 0; j < tablero[0].length; j++) {
+				if (tablero[i][j] == 0) {
+					hayCasillasVacias = true;
+				}
+			}
+		}
+
+	
+		if (hayCasillasVacias == true) {
+			return 0; 
+		} else {
+			return -1;
+		}
+	}
+
+	public void resultado() {
+		int resultado = getGanadorDelTablero(tablero);
+
+		if (resultado == -1) {
+			JOptionPane.showMessageDialog(instancia, "No quedan mas cuadros libres");
+		}
+		if (resultado == 1) {
+			JOptionPane.showMessageDialog(instancia, "Ha ganado el JUGADOR 1");
+		}
+		if (resultado == 2) {
+			JOptionPane.showMessageDialog(instancia, "Ha ganado el JUGADOR 2");
 		}
 	}
 
